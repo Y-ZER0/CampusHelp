7593019f051612f5c12d972b802acac999242a8b
Yousef Abu-nimreh <150347338+Y-ZER0@users.noreply.github.com>
2025-05-14 19:08:58 +0300

    Description of changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router , Route , Routes , Navigate } from 'react-router-dom';
 import './App.css';
 
 // Components
